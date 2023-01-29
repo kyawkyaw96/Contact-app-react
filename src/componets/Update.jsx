163e966ee15242b7b3a1dcd9398ad7edb5747180
kyawkyaw96 <winkyaw.demon@gmail.com>
2023-01-29 10:26:24 +0630

    second

--- src/componets/Update.jsx
+++ src/componets/Update.jsx
@@ -8,7 +8,6 @@ import {
 } from "../feature/api/ContactApi";
 
 const Update = () => {
-  const [loading, setLoading] = useState(true);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -37,14 +36,37 @@ const Update = () => {
           id="authentication-modal"
           tabIndex="-1"
           aria-hidden="true"
-          className="flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+          className="flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-screen"
         >
           <div className="relative w-full h-full max-w-md md:h-auto">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <div className="px-6 py-6 lg:px-8">
-                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                  Edit Here
-                </h3>
+                <div className=" flex justify-between items-center">
+                  <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                    Edit Here
+                  </h3>
+                  <button
+                    type="button"
+                    onClick={() => navigate("/")}
+                    className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                    data-modal-hide="authentication-modal"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                </div>
                 <form onSubmit={handleSubmit} className="space-y-6" action="#">
                   {/* your name */}
                   <div>
@@ -61,9 +83,7 @@ const Update = () => {
                       name="name"
                       id="name"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder={`${
-                        data?.contact?.name
-                      }`}
+                      placeholder={`${data?.contact?.name}`}
                       required
                     />
                   </div>
@@ -82,9 +102,7 @@ const Update = () => {
                       name="email"
                       id="email"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder={`${
-                        data?.contact?.email
-                      }`}
+                      placeholder={`${data?.contact?.email}`}
                       required
                     />
                   </div>
@@ -103,9 +121,7 @@ const Update = () => {
                       name="phone"
                       id="phone"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder={`${
-                        data?.contact?.phone
-                      }`}
+                      placeholder={`${data?.contact?.phone}`}
                       required
                     />
                   </div>
@@ -121,7 +137,9 @@ const Update = () => {
           </div>
         </div>
       ) : (
-        <h1 className=" flex justify-center items-center text-blue-600 h-screen text-4xl">Loading</h1>
+        <h1 className=" flex justify-center items-center text-blue-600 h-screen text-4xl">
+          Loading
+        </h1>
       )}
     </>
   );
